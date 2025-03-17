29e34d4b73442725c8aa458c2c65eb7e671e008b
Nich Galzin <nichgalzin@gmail.com>
2025-03-17 12:23:01 +0000

    basic landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="layout">
+	<header class="px-18 py-8">
+		<h1 class="prose md:prose-2xl font-display">Ashlar Co-housing</h1>
+	</header>
+	<section class="relative h-full">
+		<div
+			class="absolute
+ top-30 left-18 max-w-[500px] bg-[hsla(176.59,49.8%,51.57%,0.9)] p-8"
+		>
+			<p>
+				<em>Ashlar Collective</em> is a UK-based co-housing initiative focused on sustainable, community-led
+				living. Our community is structured around mutual ownership, environment sustainability and community.
+			</p>
+		</div>
+		<img
+			class="max-h-[668px] w-full object-cover"
+			src="/images/banner-image-1.jpg"
+			alt="Various items on a picnic table"
+		/>
+	</section>
+	<footer class="flex w-full flex-col items-center justify-between gap-3 px-18 py-8 md:flex-row">
+		<a href="mailto:ashlarcollect@gmail.com">Contact Us</a>
+		<p>Ashlar Collective Ltd.</p>
+	</footer>
+</main>
+
+<style>
+	.layout {
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		height: 100vh;
+	}
+</style>
